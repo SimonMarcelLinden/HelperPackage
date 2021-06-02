20d426f0b6ab41081b6fbcb47011db7d4c1aec55
Simon Marcel Linden <info@snerve.de>
2021-06-02 14:21:08 +0200

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,54 @@
+# HelperPackage
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Total Downloads][ico-downloads]][link-downloads]
+[![Build Status][ico-travis]][link-travis]
+
+This is where your description should go. Take a look at [contributing.md](contributing.md) to see a to do list.
+
+## Installation
+
+Via Composer
+
+``` bash
+$ composer require simonmarcellinden/helperpackage
+```
+
+## Usage
+
+## Change log
+
+Please see the [changelog](changelog.md) for more information on what has changed recently.
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Contributing
+
+Please see [contributing.md](contributing.md) for details and a todolist.
+
+## Security
+
+If you discover any security related issues, please email info@snerve.de instead of using the issue tracker.
+
+## Credits
+
+- [Simon Marcel Linden][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+MIT. Please see the [license file](license.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/simonmarcellinden/helperpackage.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/simonmarcellinden/helperpackage.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/simonmarcellinden/helperpackage/master.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/simonmarcellinden/helperpackage
+[link-downloads]: https://packagist.org/packages/simonmarcellinden/helperpackage
+[link-travis]: https://travis-ci.org/simonmarcellinden/helperpackage
+[link-author]: https://github.com/simonmarcellinden
+[link-contributors]: ../../contributors
---
+++ src/Presets/Preset.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace SimonMarcelLinden\Presets;
+
+use Illuminate\Filesystem\Filesystem;
+
+abstract class Preset {
+
+    /**
+     * Ensure the component directories we need exist.
+     *
+     * @param mixed $directories
+     * @return void
+     */
+    protected static function ensureComponentDirectoryExists($directories): void{
+        if( is_array($directories)) {
+            foreach ($directories as $directory) {
+                static::createDirectoryIfNotExists($directory);
+            }
+        } else {
+            static::createDirectoryIfNotExists($directories);
+        }
+    }
+
+    /**
+     * Make sure the directory still exists.
+     *
+     * @param String $path
+     * @return void
+     */
+    private static function createDirectoryIfNotExists(String $path): void {
+        $filesystem = new Filesystem;
+        if (! $filesystem->isDirectory($directory = resource_path( $path ))) {
+            $filesystem->makeDirectory($directory, 0755, true);
+        }
+    }
+
+    /**
+     * Update the "package.json" file.
+     *
+     * @param  bool  $dev
+     * @return void
+     */
+    protected static function updatePackages($dev = true) {
+        if (! file_exists(base_path('package.json'))) {
+            return;
+        }
+
+        $configurationKey = $dev ? 'devDependencies' : 'dependencies';
+
+        $packages = json_decode(file_get_contents(base_path('package.json')), true);
+
+        $params = array_key_exists($configurationKey, $packages) ? $packages[$configurationKey] : [];
+
+        // $packages[$configurationKey] = static::updatePackageArray($params, $configurationKey);
+        $packages[$configurationKey] = static::updatePackageArray($params);
+
+        ksort($packages[$configurationKey]);
+
+        file_put_contents( base_path('package.json'), json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL );
+    }
+
+    /**
+     * Remove the installed Node modules.
+     *
+     * @return void
+     */
+    protected static function removeNodeModules(): void {
+        tap(new Filesystem, function ($files) {
+            $files->deleteDirectory(base_path('node_modules'));
+
+            $files->delete(base_path('yarn.lock'));
+        });
+    }
+
+    /**
+     * Update the given package array.
+     *
+     * @param  array  $packages | return ['vue' => '^4.0.0',] + Arr::except($packages, ['react-dom', ]);
+     * @return array
+     */
+    abstract static function updatePackageArray(array $packages);
+}
